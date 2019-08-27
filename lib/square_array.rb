52f188cfa432bacf058848c6d5587ee38d8addb4
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 02:10:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  counter = 0
-  while array[counter] do
-    array[counter] *= array[counter]
-    counter += 1
-  end
-  array
+  # counter = 0
+  # while array[counter] do
+  #   array[counter] *= array[counter]
+  #   counter += 1
+  # end
+  # array
 end
